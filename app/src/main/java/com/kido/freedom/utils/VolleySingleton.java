bed158375392fab3487ea080d566d75f4d96a48f
tosash <tosash@gmail.com>
2015-05-27 11:49:17 +0300

    Volley

--- app/src/main/java/com/kido/freedom/utils/VolleySingleton.java
+++ app/src/main/java/com/kido/freedom/utils/VolleySingleton.java
@@ -1,36 +1,38 @@
 package com.kido.freedom.utils;
 
-import android.app.Application;
+import android.content.Context;
 import android.text.TextUtils;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
-public class AppController extends Application {
-
-	public static final String TAG = AppController.class
-			.getSimpleName();
-
+public class VolleySingleton {
+	private static VolleySingleton mInstance = null;
+	private static Context mContext;
 	private RequestQueue mRequestQueue;
 	private ImageLoader mImageLoader;
+	public static final String TAG = VolleySingleton.class
+			.getSimpleName();
 
-	private static AppController mInstance;
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		mInstance = this;
+	private VolleySingleton(Context context) {
+		mContext = context;
+		mRequestQueue = getRequestQueue();
+		mImageLoader = new ImageLoader(mRequestQueue, new BitmapLruCache());
 	}
 
-	public static synchronized AppController getInstance() {
+	public static synchronized VolleySingleton getInstance(Context context) {
+		if (mInstance == null) {
+			mInstance = new VolleySingleton(context);
+		}
 		return mInstance;
 	}
 
 	public RequestQueue getRequestQueue() {
 		if (mRequestQueue == null) {
-			mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+			mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
 		}
 
 		return mRequestQueue;
@@ -48,11 +50,20 @@ public class AppController extends Application {
 	public <T> void addToRequestQueue(Request<T> req, String tag) {
 		// set the default tag if tag is empty
 		req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+		req.setRetryPolicy(new DefaultRetryPolicy(30000,
+				DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+				DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+		req.setShouldCache(true);
 		getRequestQueue().add(req);
+
 	}
 
 	public <T> void addToRequestQueue(Request<T> req) {
 		req.setTag(TAG);
+		req.setRetryPolicy(new DefaultRetryPolicy(30000,
+				DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+				DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+		req.setShouldCache(true);
 		getRequestQueue().add(req);
 	}
 
